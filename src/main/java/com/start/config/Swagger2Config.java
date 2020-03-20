ec62fae3b7601ef7dd783df7ae3c493b644181d5
Artem Fedotov <a.fedotov@andersenlab.com>
2020-03-20 15:44:55 +0200

    swagger config

--- src/main/java/com/start/config/Swagger2Config.java
+++ src/main/java/com/start/config/Swagger2Config.java
@@ -2,16 +2,21 @@ package com.start.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
+public class Swagger2Config implements WebMvcConfigurer {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -19,11 +24,23 @@ public class Swagger2Config {
 				.apis(RequestHandlerSelectors
 						.basePackage("com.start.controller"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(getApiInfo());
 	}
 
-	@Bean
-	public Swagger2DocumentationConfiguration swagger2DocumentationConfiguration() {
-		return new Swagger2DocumentationConfiguration();
+	private ApiInfo getApiInfo() {
+		return new ApiInfo("Swagger2 Api Documentation",
+				"How to generate Swagger documentation for your Rest API",
+				"1.0", "urn:tos",
+				new Contact("Bannes", "www.start.com", "artemfedotov045@gmail.com"),
+				"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//enabling swagger-ui part for visual documentation
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
 }
